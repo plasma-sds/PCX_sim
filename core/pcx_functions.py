cacc19d1623954aa08abb40f316c4c1b6c71eaa4
Balazs-Peter <balazs1211@gmail.com>
2026-02-10 12:44:45 +0100

    Implemented n-resolved calculation for He+.

--- core/pcx_functions.py
+++ core/pcx_functions.py
@@ -210,38 +210,28 @@ class PCX_run_dev:
         self.v_electron = VelocityDistribution(self.T_plasma, Ae, v0 = self.v_He)
         self.v_cloud = VelocityDistribution(self.T_H, self.m_H, v0 = self.v_He)
 
-        self.calculate_RateCoefficientMatrix()
+        self.calculate_rate_coefficients()
+        self.assemble_rate_coefficient_matrix()
+        self.get_reaction_masks()
         
-        self.dn_He2_mask = np.array([[0, 0, 0, 0],
-                                    [1, 1, 0, 1],
-                                    [0, 0, -1, 0]])
-        
-        self.dn_He1_mask = np.array([[1, 1, 0, 1],
-                                    [-1,-1,-1,-1],
-                                    [0, 0, 1, 0]])
-        
-        self.dn_He_mask = np.array([[-1,-1, 0, -1],
-                                    [0, 0, 1, 0],
-                                    [0, 0, 0, 0]])
 
-        self.n_He = np.empty((3,self.N), dtype=float)
-        self.n_He[:,0] = np.array([0.0, 0.0, n_He0])
-        n_reactants = np.array([self.n_plasma, self.n_plasma, self.n_H, self.n_H])
+        self.n_He = np.empty((7,self.N), dtype=float)
+        self.n_He[:, 0] = 0.0
+        self.n_He[-1,0] = n_He0
+        n_reactants = np.vstack((5*[self.n_plasma], 5*[self.n_plasma], 5*[self.n_H], 5*[self.n_H]))
 
         for i in range(1, self.N):
 
-            dn_He = self.n_He[:,i-1].dot((self.dn_He_mask*self.rate_coeff_matrix).dot(n_reactants[:,i-1]))
-            self.n_He[0, i] = min(max(self.n_He[0, i-1] + dn_He*self.dt, 0), n_He0)
+            dn_He = np.zeros(len(self.reaction_masks), dtype=float)
+            for j,mask in enumerate(self.reaction_masks):
+                dn_He[j] = self.n_He[:,i-1].dot((mask*self.rate_coeff_matrix).dot(n_reactants[:,i-1]))
 
-            dn_He1 = self.n_He[:,i-1].dot((self.dn_He1_mask*self.rate_coeff_matrix).dot(n_reactants[:,i-1]))
-            self.n_He[1, i] = min(max(self.n_He[1,i-1] + dn_He1*self.dt, 0), n_He0)
-
-            dn_He2 = self.n_He[:,i-1].dot((self.dn_He2_mask*self.rate_coeff_matrix).dot(n_reactants[:,i-1]))
-            self.n_He[2, i] = min(max(self.n_He[2, i-1] + dn_He2*self.dt, 0), n_He0)
+            self.n_He[:, i] = np.clip(self.n_He[:, i-1] + dn_He*self.dt, a_min=0, a_max=n_He0)
 
 
 
-    def calculate_RateCoefficientMatrix(self):
+
+    def calculate_rate_coefficients(self):
 
         #from He+ to He2+
         self.R_He1_e_ion = calculate_rate_coeff(self.v_electron.v_values, self.cross_sections.get_cross_section('electron', 'He+', 'He2+').cross_section(self.v_electron.E_values), self.v_electron.f_v)
@@ -261,7 +251,135 @@ class PCX_run_dev:
                                             calculate_rate_coeff(self.v_ion.v_values, self.cross_sections.get_cross_section('H+', 'He', 'cxHe+').cross_section(self.v_ion.E_values), self.v_ion.f_v)
         self.R_He_H_ion = calculate_rate_coeff(self.v_cloud.v_values, self.cross_sections.get_cross_section('H', 'He', 'He+').cross_section(self.v_cloud.E_values), self.v_cloud.f_v)
 
-                                            #electron           #ion                #cloud CX     #cloud ionization
-        self.rate_coeff_matrix = np.array([[self.R_He_e_ion, self.R_He_p_loss,   0,               self.R_He_H_ion],  #He
-                                          [self.R_He1_e_ion, self.R_He1_p_loss, self.R_He1_H_cx, self.R_He1_H_ion],  #He1
-                                          [0,                0,                 self.R_He2_H_cx, 0               ]]) #He2
+        self.R_He1_e_ex = self.R_He1_p_ex = self.R_He1_H_ex = 1e-16
+
+        #                                     #electron           #ion                #cloud CX     #cloud ionization
+        # self.rate_coeff_matrix = np.array([[self.R_He_e_ion, self.R_He_p_loss,   0,               self.R_He_H_ion],  #He
+        #                                   [self.R_He1_e_ion, self.R_He1_p_loss, self.R_He1_H_cx, self.R_He1_H_ion],  #He1
+        #                                   [0,                0,                 self.R_He2_H_cx, 0               ]]) #He2
+        
+
+        # electron_matrix = np.array([[self.R_He_e_ion/5,  self.R_He_e_ion/5,  self.R_He_e_ion/5,  self.R_He_e_ion/5, self.R_He_e_ion/5],
+        #                             [self.R_He1_e_ion/5, self.R_He1_e_ex,    self.R_He1_e_ex,    self.R_He1_e_ex,   self.R_He1_e_ex],
+        #                             [self.R_He1_e_ex,    self.R_He1_e_ion/5, self.R_He1_e_ex,    self.R_He1_e_ex,   self.R_He1_e_ex],
+        #                             [self.R_He1_e_ex,    self.R_He1_e_ex,    self.R_He1_e_ion/5, self.R_He1_e_ex,   self.R_He1_e_ex],
+        #                             [self.R_He1_e_ex,    self.R_He1_e_ex,    self.R_He1_e_ex,    self.R_He1_e_ion/5, self.R_He1_e_ex],
+        #                             [self.R_He1_e_ex,    self.R_He1_e_ex,    self.R_He1_e_ex,    self.R_He1_e_ex,   self.R_He1_e_ion/5,]
+        #                             [0.0,                0.0,                0.0,                0.0,               0.0]])
+
+
+    def assemble_rate_coefficient_matrix(self):
+
+        projectiles = ['He', 'He1', 'He1', 'He1', 'He1', 'He1', 'He2']
+        states = [None, 1, 2, 3, 4, 5, None]
+        #targets = ['e', 'p', 'Hi', 'Hd']
+
+        self.electron_matrix = self.get_rate_coeff_matrix_for_target(projectiles, states, 'e')
+        self.proton_matrix = self.get_rate_coeff_matrix_for_target(projectiles, states, 'p')
+        self.Hi_matrix = self.get_rate_coeff_matrix_for_target(projectiles, states, 'Hi')
+        self.Hd_matrix = self.get_rate_coeff_matrix_for_target(projectiles, states, 'Hd')
+
+        self.rate_coeff_matrix = np.hstack((self.electron_matrix, self.proton_matrix, self.Hi_matrix, self.Hd_matrix))
+
+    def get_rate_coeff_matrix_for_target(self, projectiles, states, target):
+        matrix = np.zeros((7, 5), dtype=float)
+        for i,proj in enumerate(projectiles):
+            for j in range(5):
+                matrix[i,j] = self.select_rate_coefficient(He_ion=proj, He_state=states[i], target=target, target_ind=j+1)
+        return matrix
+
+    def select_rate_coefficient(self, He_ion, He_state, target, target_ind):
+
+        if He_ion == 'He' and not He_state:
+            if target == 'e':
+                return self.R_He_e_ion/5
+            elif target == 'p':
+                return self.R_He_p_loss/5
+            elif target == 'Hi':
+                return self.R_He_H_ion/5
+            else:
+                return 0.0
+            
+        elif He_ion == 'He1' and He_state:
+            if target == 'e':
+                if target_ind == He_state:
+                    return self.R_He1_e_ion
+                else:
+                    return self.R_He1_e_ex
+            elif target == 'p':
+                if target_ind == He_state:
+                    return self.R_He1_p_loss
+                else:
+                    return self.R_He1_p_ex
+            elif target == 'Hi':
+                if target_ind == He_state:
+                    return self.R_He1_H_ion
+                else:
+                    return self.R_He1_H_ex
+            elif target == 'Hd':
+                if target_ind == 1:
+                    return self.R_He1_H_cx
+                else:
+                    return 0.0
+                
+        elif He_ion == 'He2' and not He_state:
+            if target == 'Hd':
+                return self.R_He2_H_cx/5
+            else:
+                return 0.0
+
+    def get_reaction_masks(self):
+        nmax = 5
+        self.reaction_masks = [self.assemble_mask(He_charge=0, nmax=nmax),
+                               self.assemble_mask(He_charge=1, nmax=nmax, nstate=1),
+                               self.assemble_mask(He_charge=1, nmax=nmax, nstate=2),
+                               self.assemble_mask(He_charge=1, nmax=nmax, nstate=3),
+                               self.assemble_mask(He_charge=1, nmax=nmax, nstate=4),
+                               self.assemble_mask(He_charge=1, nmax=nmax, nstate=5),
+                               self.assemble_mask(He_charge=2, nmax=nmax)]
+
+    def assemble_mask(self, He_charge, nmax, nstate = None):
+        if He_charge == 0 and not nstate:
+            ion = He_ion_mask(nmax)
+            cx = He_cx_mask(nmax)
+        elif He_charge == 2 and not nstate:
+            ion = He2_ion_mask(nmax)
+            cx = He2_cx_mask(nmax)
+        elif He_charge == 1 and nstate:
+            ion = He1_i_ion_ex_mask(nmax, nstate)
+            cx = He1_i_cx_mask(nmax, nstate)
+        mask = np.hstack((ion, ion, ion, cx))
+        return mask
+        
+def He_ion_mask(n):
+    mask = np.zeros((n+2, n), dtype=float)
+    mask[0] = -1
+    return mask
+
+def He1_i_ion_ex_mask(n, i):
+    mask = np.zeros((n+2, n), dtype=float)
+    mask[:-1,i-1] = 1
+    mask[i] = -1
+    return mask
+
+def He2_ion_mask(n):
+    mask = np.zeros((n+2, n), dtype=float)
+    r = np.arange(n)
+    mask[r+1,r] = 1
+    return mask
+
+def He_cx_mask(n):
+    mask = np.zeros((n+2, n), dtype=float)
+    mask[1:-1,0] = 1
+    return mask
+
+def He1_i_cx_mask(n,i):
+    mask = np.zeros((n+2, n), dtype=float)
+    mask[i,0] = -1
+    mask[-1,i-1] = 1
+    return mask
+
+def He2_cx_mask(n):
+    mask = np.zeros((n+2, n), dtype=float)
+    mask[-1] = -1
+    return mask
